eec85390753f82339a4ecc447bc27f0761080dcd
catalinbanu <cata_banu98@yahoo.com>
2021-03-27 13:35:14 +0200

    Implement reading sensors

--- Server/Server.ino
+++ Server/Server.ino
@@ -13,12 +13,17 @@ EthernetClient client;
 
 DHT dht(2,DHT11);
 
-const size_t CAPACITY = JSON_OBJECT_SIZE(2);
+const size_t CAPACITY = JSON_OBJECT_SIZE(5);
 StaticJsonDocument<CAPACITY> doc;
 
 char output[200];
 float set_temperature = 21.5;
-float set_soil_moisture = 20;
+float set_soil_moisture = 60;
+int trigPin = 9;   
+int echoPin = 8;    
+
+float duration_us, level;
+
 
 void set_regulation(float temp, float set_temp, byte actuator)
 {
@@ -28,6 +33,14 @@ void set_regulation(float temp, float set_temp, byte actuator)
   if( temp < set_temp){
     digitalWrite(actuator,HIGH);
   }
+}
+float getWaterLevel(){
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration_us = pulseIn(echoPin, HIGH);
+  level = 0.017 * duration_us;
+  return level;
 }
 
 void setup() {
@@ -40,19 +53,30 @@ void setup() {
 
   pinMode(COOLER,OUTPUT);
   digitalWrite(COOLER,HIGH);
+  pinMode(WATER_PUMP,OUTPUT);
+  digitalWrite(WATER_PUMP,HIGH);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
 
   float h = dht.readHumidity( );
   float t = dht.readTemperature( );
+  int soilMoistureInput = analogRead(A0);
+  int soilMoisture = map(soilMoistureInput,200,1100,100,0);
+  int light = analogRead(A1);
 
   set_regulation(t,set_temperature,COOLER);
+  set_regulation(soilMoisture,set_soil_moisture,WATER_PUMP);
 
   JsonObject object = doc.to<JsonObject>();
 
   object["temperature"] = t;
   object["humidity"] = h;
+  object["soil_moisture"] = soilMoisture;
+  object["light"] = light;
+  object["water_level"] = getWaterLevel();
 
   serializeJsonPretty(object,output);
 
