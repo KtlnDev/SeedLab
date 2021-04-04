ee2c8fab20253611f036d05d80647f2af16d94c6
catalinbanu <cata_banu98@yahoo.com>
2021-04-04 14:05:55 +0300

    Create routing for main page

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Navigation from './components/nav-bar/navbar.component'
+import WelcomeComponent from './components/welcome/welcome.component'
+import LoginComponent from './components/login/login.component'
 import RegisterComponent from './components/register/register.component'
 
 function App() {
   return (
-    <div className="App">
-      <RegisterComponent/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={WelcomeComponent}/>
+          <Route path="/login" component={LoginComponent}/>
+          <Route path="/register" component={RegisterComponent}/>
+        </Switch>
+      </div>    
+    </Router>
   );
 }
 
