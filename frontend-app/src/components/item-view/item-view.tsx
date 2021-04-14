7a5890c3435e8a8c9ac5042f97d60a30975518c7
catalinbanu <cata_banu98@yahoo.com>
2021-04-14 21:13:56 +0300

    Implement item-view

--- frontend-app/src/components/item-view/item-view.tsx
+++ frontend-app/src/components/item-view/item-view.tsx
@@ -4,9 +4,19 @@ import './item-view.scss'
 export default function ItemView(props){
     return(
         <div className="item-view">
-            <label>{props.name}</label><br>
-            </br>
-            <label>{props.value}</label>
+            <div className="name">
+                <label>{props.name}</label>
+            </div>
+            <div className="value-container">
+                <div className="left-side">
+                    <div className="value-unit">
+                        <label className="value">{props.value}</label>
+                        <label className="unit">{props.unit}</label>
+                    </div> 
+                    <label className="extraInfo">Cooler Status: OFF</label>                   
+                </div>
+                <img src={props.icon}  className="icon-design" alt="temp"/>
+            </div>
         </div>
     )
 }
