7a1a298fe37d1634f2c4871416d67f81435fc399
catalinbanu <cata_banu98@yahoo.com>
2021-04-18 13:16:10 +0300

    Handling command from client [Arduino]

--- Server/Server.ino
+++ Server/Server.ino
@@ -5,9 +5,9 @@
 
 #define COOLER 3
 #define WATER_PUMP 4
+#define LIGHT_BULB 5
 #define echoPin 8
 #define trigPin 9
-
 
 byte mac[]={0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192,168,1,10);
@@ -16,29 +16,53 @@ EthernetClient client;
 
 DHT dht(2,DHT11);
 
-const size_t CAPACITY = JSON_OBJECT_SIZE(5);
-StaticJsonDocument<CAPACITY> doc;
+const size_t capacity_out = JSON_OBJECT_SIZE(8);
+StaticJsonDocument<capacity_out> docOut;
+
+const size_t capacity_in = JSON_OBJECT_SIZE(3);
+DynamicJsonDocument docIn(capacity_in);
 
 char output[200];
+char command[200];
 float duration_us, level;
-float set_temperature = 23;
-float set_soil_moisture = 60;
+float set_temperature;
+float set_soil_moisture;
+int control_light;
+
+int cooler_status;
+int water_pump_status;
+int light_bulb_status;
 
 void temperature_regulation(float temp, float set_temp){
   if( temp >= set_temp){
     digitalWrite(COOLER,LOW);
+    cooler_status = 1;
   }
   if( temp < set_temp){
     digitalWrite(COOLER,HIGH);
+    cooler_status = 0;
+  }
+}
+
+void switchLight( int lightIndicator){
+  if(lightIndicator == 1){
+    digitalWrite(LIGHT_BULB,LOW);
+    light_bulb_status = 1;
+  }
+  else{
+    digitalWrite(LIGHT_BULB,HIGH);
+    light_bulb_status = 0;
   }
 }
 
 void soil_moisture_regulation(float soil_moisture, float set_soil_moisture){
   if( soil_moisture < set_soil_moisture){
     digitalWrite(WATER_PUMP,LOW);
+    water_pump_status = 1;
   }
   if( soil_moisture >= set_soil_moisture){
     digitalWrite(WATER_PUMP,HIGH);
+    water_pump_status = 0;
   }
 }
 
@@ -63,6 +87,8 @@ void setup() {
   digitalWrite(COOLER,HIGH);
   pinMode(WATER_PUMP,OUTPUT);
   digitalWrite(WATER_PUMP,HIGH);
+  pinMode(LIGHT_BULB,OUTPUT);
+  digitalWrite(LIGHT_BULB,HIGH);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
@@ -75,16 +101,16 @@ void loop() {
   int soilMoisture = map(soilMoistureInput,200,1100,100,0);
   int light = analogRead(A1);
 
-  temperature_regulation(t,set_temperature);
-  soil_moisture_regulation(soilMoisture,set_soil_moisture);
-
-  JsonObject object = doc.to<JsonObject>();
+  JsonObject object = docOut.to<JsonObject>();
 
   object["temperature"] = t;
   object["humidity"] = h;
   object["soil_moisture"] = soilMoisture;
   object["light"] = light;
   object["water_level"] = getWaterLevel();
+  object["cooler_status"] = cooler_status;
+  object["water_pump_status"] = water_pump_status;
+  object["light_bulb_status"] = light_bulb_status;
 
   serializeJsonPretty(object,output);
 
@@ -92,17 +118,30 @@ void loop() {
   if (client) 
    {
       boolean currentLineIsBlank = true;
-      while (client.connected ( ) ) 
+      while (client.connected()) 
         {
-          if (client.available ( ) ) 
+          if (client.available()) 
             {
-              char character = client.read ( );
+              char character = client.read();
               Serial.write(character);
               if (character == '\n' && currentLineIsBlank) 
                 {
+                  int index = 0;
                   while(client.available()){
-                    Serial.write(client.read());
+                     command[index] = client.read();
+                     index++;
                   }
+                      
+                  DeserializationError err = deserializeJson(docIn, command);
+                  if (err) {
+                    Serial.print(F("deserializeJson() failed with code "));
+                    Serial.println(err.c_str());
+                  }
+
+                  set_temperature= docIn["temperature"];
+                  set_soil_moisture = docIn["soilMoisture"];
+                  control_light = docIn["light"];
+                                    
                   client.println ("HTTP/1.1 200 OK");
                   client.println ("Content-Type: application/json");
                   client.println("Access-Control-Allow-Origin: *");
@@ -125,5 +164,8 @@ void loop() {
     }
     delay(1);
     client.stop();
-    }
+  }
+  temperature_regulation(t,set_temperature);
+  soil_moisture_regulation(soilMoisture,set_soil_moisture);
+  switchLight(control_light);
 }
