a48c1ec982476ec2b4969470b928bf6a0ab23a96
catalinbanu <cata_banu98@yahoo.com>
2021-05-06 13:05:06 +0300

    Update error message and returning data from backend side

--- backend-app/routes/auth.js
+++ backend-app/routes/auth.js
@@ -3,22 +3,31 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../model/User');
 const emailValidator = require('email-validator');
+const jwt_decode = require('jwt-decode');
 
 router.post('/register', async (req,res) => {
 
     const { firstName, lastName, email, password, confirmPassword} = req.body;
     
     if(!emailValidator.validate(email))
-        return res.status(400).json("Email address is not valid!")
+        return res.status(400).json({
+            errorMessage:"Email address is not valid!"
+        });
     
     if(password.length < 6)
-        return res.status(400).json("Please enter a password of at least 6 characters")
+        return res.status(400).json({
+            errorMessage:"Please enter a password of at least 6 characters!"
+        });
     
     if(password !== confirmPassword)
-        return res.status(400).json("Wrong confirmation password!")
+        return res.status(400).json({
+            errorMessage:"Your password and confirmation password do not match!"
+        });
     
     const existingUser = await User.findOne({email});
-    if(existingUser) return res.status(400).json("Email already exists!")
+    if(existingUser) return res.status(400).json({
+        errorMessage:"Email already exists!"
+    });
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -31,7 +40,9 @@ router.post('/register', async (req,res) => {
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.status(201).send({
+            message: "Account created!"
+        });
     }
     catch(err){
         res.status(400).send(err);
@@ -43,21 +54,30 @@ router.post('/login', async (req,res) => {
         const{ email, password} = req.body;
 
         if(!emailValidator.validate(email))
-            return res.status(400).json("Email address is not valid!")
+            return res.status(400).json({
+                errorMessage:"Email address is not valid!"
+            });
 
         const existingUser = await User.findOne({email});
 
         if(!existingUser) 
-            return res.status(401).json("Email address is not exists!");
+            return res.status(404).json({
+                errorMessage:"Email address is not exists!"
+            });
         
         const verifyPassword = await bcrypt.compare(req.body.password, existingUser.password);
         if(!verifyPassword)
-            return res.status(401).json("Wrong password");
+            return res.status(400).json({
+                errorMessage:"Your password is incorrect!"
+            });
         
         const token = jwt.sign({_id: existingUser._id, firstName: existingUser.firstName, lastName: existingUser.lastName}, process.env.TOKEN_SECRET);
             res.cookie('access_token',token,{
                 httpOnly: true,
-            }).send();
+            }).send({
+                isAuthenticated: true,
+                username: existingUser.firstName + ' ' + existingUser.lastName
+            });
 
     }catch(err){
         res.status(500).send(err);
@@ -68,19 +88,29 @@ router.get('/logout', async (req,res) => {
     res.cookie('access_token',"",{
         httpOnly: true,
         expires: new Date(0)
-    }).send();
+    }).send("Logout");
 });
 
 router.get('/loggedIn', (req,res) => {
     try{
         const token = req.cookies.access_token;
 
-        if(!token) 
-            return res.status(401).json(false);
-        res.send(true);
+        if(token){
+            const user = jwt_decode(token);
+            const username = user.firstName + ' ' + user.lastName;
 
+            res.send({
+                isAuthenticated: true,
+                username: username
+            });
+        } else {
+            return res.status(401).send({
+                isAuthenticated: false,
+                username: null
+            });
+        }
     }catch(err){
-        res.json(false);
+        res.send(err);
     }
 });
 
