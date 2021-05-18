3bcc868637fd21acb8542ab3bb179a164c4ab0a1
catalinbanu <cata_banu98@yahoo.com>
2021-05-18 21:01:17 +0300

    Change layout for item view

--- frontend-app/src/components/item-view/item-view.tsx
+++ frontend-app/src/components/item-view/item-view.tsx
@@ -5,17 +5,16 @@ export default function ItemView(props){
     return(
         <div className="item-view">
             <div className="name">
-                <label>{props.name}</label>
+                <label>
+                     {props.name} 
+                </label>
             </div>
             <div className="value-container">
-                <div className="left-side">
-                    <div className="value-unit">
-                        <label className="value">{props.value}</label>
-                        <label className="unit">{props.unit}</label>
-                    </div> 
-                    <label className="extraInfo">{props.extraInfo}</label>                   
+                <div className="value-unit">
+                    <label className="value">{props.value}</label>
+                    <label className="unit">{props.unit}</label> 
                 </div>
-                <img src={props.icon}  className="icon-design" alt="temp"/>
+                <img src={props.icon}  className="icon-design" alt="Not found"/>
             </div>
         </div>
     )
